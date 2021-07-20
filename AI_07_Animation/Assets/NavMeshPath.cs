267f03794d6d95d32a37e2dc0b39c0c5a1b6e375
CarlosPeña <36167348+CarlosUPC@users.noreply.github.com>
2021-07-20 13:24:01 +0200

    ai update

---
+++ AI_07_Animation/Assets/NavMeshPath.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshPath : MonoBehaviour
+{
+    Move move;
+    SteeringSeek seek;
+
+    public GameObject target;
+    private UnityEngine.AI.NavMeshPath path;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        move = GetComponent<Move>();
+        seek = GetComponent<SteeringSeek>();
+
+        path = new UnityEngine.AI.NavMeshPath();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NavMesh.CalculatePath(transform.position, target.transform.position, NavMesh.AllAreas, path);
+
+        for (int i = 0; i < path.corners.Length - 1; i++)
+            Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
+
+        if (path.corners.Length >= 1)
+        {
+            seek.Steer(path.corners[1]);
+        }
+        else
+        {
+            seek.Steer(transform.position);
+        }
+
+     
+    }
+}
